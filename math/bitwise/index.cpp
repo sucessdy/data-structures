2c5bb4bec46cc0b4c814c4d1928022df192c1b3a
Mukta <sucessdy@gmail.com>
2026-02-17 22:04:40 +0530

    added single number

--- math/bitwise/index.cpp
+++ math/bitwise/index.cpp
@@ -1,48 +1,113 @@
 #include <iostream>
-#include <cmath> 
-using namespace std; 
-bool odd(int num){
-    return (num & 1 ) == 1; 
+#include <cmath>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+// bool odd(int num)
+// {
+//     return (num & 1) ==1 ;
+// }
 
- }
+int appearOnce(vector<int> num)
+{
 
- int appearOnce(int num){
+    int ans = 0;
 
- }
-int main() { 
-    // decimal to binary 
-    int n = 13; 
-    string bin = ""; 
-
-    while (n > 0){ 
-bin = to_string(n % 2) + bin ;
-n/= 2; 
-
+    for (int i = 0; i < num.size(); ++i)
+    {
+        ans ^= num[i];
     }
-    cout << bin  << endl ;  
-
-    // binary to decimal 
-
-string binary = "10101"; 
-int result = 0;
-int power = 0; 
-for (int i = binary.length() -1 ; i >= 0 ; i--){ 
-    result += (binary[i] - '0')  * pow(2, power); 
-power ++; 
+    return ans;
 }
-cout << result << endl; 
+
+int appearOncewithBruteForce(vector<int> &num)
+{
+
+    sort(num.begin(), num.end());
+
+    int ans = 0;
+
+    for (int i = 0; i < num.size(); ++i)
+    {
+        if (num[i] != num[i - 1] && num[i] != num[i + 1])
+        {
+            ans = num[i];
+            return ans;
+        }
+
+        if (num[i - 1] != num[i - 2])
+        {
+            return num[i - 1];
+        }
+    }
+    return 0;
+}
+int appearOnceBetter ( vector<int> &num ) {
+    unordered_map<int, int> m; 
+for ( int i = 0 ; i < num.size() ; i ++ ){
+     m[num[i]]++ ; 
 
 
+}
+for ( auto it = m.begin() ; it != m.end() ; it++){
+    if (it->second == 1){
+        return it->first ; 
+    }
+}
+return -1;
+}
 
-int num = 23 ; 
-// if (num & 1){
-//     cout << "ODD" ;
-// }
-// else { 
-//     cout << "EVEN " ; 
-// }
-cout << odd(num) << endl;
+bool PowerofTwo(int n ){
+    return n > 0 && (n & (n -1) ) == 0 ; 
+
+ }
 
 
-    return 0; 
+int main()
+{
+    int n = 1; 
+   
+bool check = PowerofTwo(n); 
+if (check){ 
+    cout << "True" << "\n"; 
+
+}
+else { 
+    cout << "false"  << "\n"; 
+}
+    vector<int> a = {1, 1, 3, 3, 4 , 66};
+    // cout << appearOncewithBruteForce(a) << endl;
+    // cout << appearOnceBetter(a) << endl; 
+    // decimal to binary
+    //     int n = 13;
+    //     string bin = "";
+
+    //     while (n > 0){
+    // bin = to_string(n % 2) + bin ;
+    // n/= 2;
+
+    //     }
+    //     cout << bin  << endl ;
+
+    //     // binary to decimal
+
+    // string binary = "10101";
+    // int result = 0;
+    // int power = 0;
+    // for (int i = binary.length() -1 ; i >= 0 ; i--){
+    //     result += (binary[i] - '0')  * pow(2, power);
+    // power ++;
+    // }
+    // cout << result << endl;
+
+    // int num = 23 ;
+    // if (num >> 1){
+    //     cout << "ODD" ;
+    // }
+    // else {
+    //     cout << "EVEN " ;
+    // }
+    // cout << odd(num) << endl;
+
+    return 0;
 }
---
+++ math/bitwise/main.cpp
@@ -0,0 +1,52 @@
+// find the position of the right most set bit 
+#include <iostream>
+#include <vector> 
+
+using namespace std; 
+
+// int singleNumberThrice(vector<int> &nums) { 
+
+
+// int res = 0; 
+// for (int i = 0; i < 32 ;i ++){
+// int cnt =0; 
+//     for ( auto i : num){
+// if (num & (1 << i)){ 
+//      cnt++; 
+// } 
+
+//     }
+//     if (cnt % 3 ==1 ){
+//         res |= (1 << i) ; 
+//     }
+//     return res; 
+
+// }
+
+
+// }
+
+
+int countSetBit(int n ){
+    int cnt = 0 ; 
+    while (n > 0){ 
+        cnt+= (n& 1); 
+        n = n >> 1 ;
+
+    }
+    return cnt; 
+}
+
+int main (){ 
+
+
+    int n= 13; 
+    cout << countSetBit(n) << endl; 
+    unsigned int num = 25; 
+
+    unsigned int mask = (1 << 0) |  (1 << 2 ) | (1 << 3) ; 
+num ^=mask; 
+cout << num << endl; 
+return 0; 
+}
+

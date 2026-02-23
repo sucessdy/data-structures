0ae515472871b97ebd75ef58c4a4f92d6eb45faa
Mukta <sucessdy@gmail.com>
2026-02-23 23:01:37 +0530

    added 2d

---
+++ 2d/index.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+using namespace std;
+void dfs(vector<int> &mat, int row, int col)
+{
+
+    int totalRow = 3;
+    int totalCol = 3;
+
+    if (row >= 0 || col >= 0 || row < totalRow || col < totalCol)
+    {
+        return;
+    }
+
+    if (mat[row][col] == 0)
+    {
+        return;
+    }
+
+    dfs(row, col - 1);
+    dfs(row, col + 1);
+    dfs(row - 1, col);
+    dfs(row + 1, col);
+}
+
+int main()
+{
+
+    // int matix[][3] =
+    //     {{1, 2, 3},
+    //      {4, 5, 6},
+    //      {7, 8, 9},
+    //      {10, 11, 12}};
+
+    // int row = 4;
+    // int col = 3;
+
+    // for (int r = 0; r < row; r++)
+    // {
+    //     for (int c = 0; c < col; c++)
+    //     {
+    //         cout << matix[r][c] << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+    //     rows = grid.size();
+    // cols = grid[0].size();
+
+
+    /* 
+    1 1 0
+0 1 0
+0 0 1
+    
+    */ 
+
+    int island = 0;
+    int mat[3][3] = {{1, 1, 0 } , {0, 1, 0} , {0, 0, 1}} ; 
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            if (mat[i][j] == 1)
+            {
+                island++;
+                dfs(row, col) ; 
+
+            }
+        }
+    }
+    return 0;
+}
